c4a02303bf0ec2f9d81afa55193b5576ff576b24
B3nM01 <80954614+B3nM01@users.noreply.github.com>
2021-05-26 19:36:24 +0200

    Update notipy.py

--- plugins/notipy.py
+++ plugins/notipy.py
@@ -6,11 +6,11 @@ from lightning import Plugin
 plugin = Plugin()
 @plugin.init()
 
-
+#change yourpath in this file with your path to friendlight
 
 
 def init(options, configuration, plugin):
-    f=open("/home/light/webApp/clightning.log", "a+")
+    f=open("youtpath/friendlyght/clightning.log", "a+")
     f.write("C-lightning restarted \n")
 
 
@@ -18,37 +18,37 @@ def init(options, configuration, plugin):
 
 @plugin.subscribe("connect")
 def on_connect(plugin, id, address):
-    f=open("/home/light/webApp/clightning.log", "a+")
+    f=open("youtpath/friendlyght/clightning.log", "a+")
     f.write("Connected to "+id+"\n")
 
 @plugin.subscribe("disconnect")
 def on_disconnect(plugin, id):
-    f=open("/home/light/webApp/clightning.log", "a+")
+    f=open("youtpath/friendlyght/clightning.log", "a+")
     f.write("Disconnected from "+id+"\n")
 
 @plugin.subscribe("channel_opened")
 def on_channel_opened(plugin, channel_opened):
-    f=open("/home/light/webApp/clightning.log", "a+")
+    f=open("youtpath/friendlyght/clightning.log", "a+")
     f.write("Channel opened with "+channel_opened["id"]+" \n")
 
 @plugin.subscribe("channel_open_failed")
 def on_channel_open_failed(plugin, channel_open_failed):
-    f=open("/home/light/webApp/clightning.log", "a+")
+    f=open("youtpath/friendlyght/clightning.log", "a+")
     f.write("Failed to open the channel \n")
 
 @plugin.subscribe("sendpay_success")
 def on_sendpay_success(plugin, sendpay_success):
-    f=open("/home/light/webApp/clightning.log", "a+")
+    f=open("youtpath/friendlyght/clightning.log", "a+")
     f.write("Payment success msatoshi: "+sendpay_success["msatoshi_sent"]+" \n")
 
 @plugin.subscribe("sendpay_failure")
 def on_sendpay_failure(plugin,sendpay_failure):
-    f=open("/home/light/webApp/clightning.log", "a+")
+    f=open("youtpath/friendlyght/clightning.log", "a+")
     f.write("Payment failed\n")
 
 @plugin.subscribe("invoice_payment")
 def on_invoice_creation(plugin, invoice_payment):
-    f=open("/home/light/webApp/clightning.log", "a+")
+    f=open("youtpath/friendlyght/clightning.log", "a+")
     f.write("Payment receive for label: "+invoice_payment["label"]+" \n")
 
 
